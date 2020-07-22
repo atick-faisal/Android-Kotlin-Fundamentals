6fe6d942f31480baa1e450780ea388e4fbf93272
Atick Faisal <atickfaisal@gmail.com>
2020-07-22 23:07:35 +0600

    about me done

--- AboutMe/app/src/main/java/ai/atick/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/ai/atick/aboutme/MainActivity.kt
@@ -1,12 +1,41 @@
 package ai.atick.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edit_nickname_layout.setEndIconOnClickListener { view ->
+            edit_nickname_layout.visibility = View.GONE
+            text_nickname.text = edit_nickname.text.toString()
+            text_nickname.visibility = View.VISIBLE
+            hideKeyboard(view)
+        }
+
+        text_nickname.setOnClickListener { view ->
+            view.visibility = View.GONE
+            edit_nickname_layout.visibility = View.VISIBLE
+            edit_nickname.requestFocus()
+            showKeyboard()
+        }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun showKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(edit_nickname, 0)
     }
 }
